3e9dae806da37791707b559627c83d7fcdc71b34
Udayan <udayans0107@gmail.com>
2023-08-14 12:49:02 +0530

    Linked List Questions

---
+++ LinkedList/Cycle.java
@@ -0,0 +1,24 @@
+package LinkedList;
+
+public class Cycle {
+    public boolean hasCycle(ListNode head){
+        if(head == null){
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null){
+
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ LinkedList/MergeSortedLists.java
@@ -0,0 +1,7 @@
+package LinkedList;
+
+public class MergeSortedLists {
+    public ListNode mergeTwoLists(ListNode h1, ListNode h2){
+        return null;
+    }
+}
